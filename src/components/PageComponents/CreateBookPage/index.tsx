18046f427443daa55a67e6b4ee552991c6e117f1
Sayand Sathish <sayandcode@gmail.com>
2023-10-28 19:43:21 +0530

    Use react calendar in add book form
    This date picker looks better, and even has accessibility

--- src/components/PageComponents/CreateBookPage/index.tsx
+++ src/components/PageComponents/CreateBookPage/index.tsx
@@ -1,6 +1,5 @@
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
-import { Label } from '@/components/ui/label';
 import { zodResolver } from '@hookform/resolvers/zod';
 import Head from 'next/head'
 import Link from 'next/link';
@@ -10,11 +9,15 @@ import { z } from 'zod';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Popover, PopoverTrigger, PopoverContent } from '@/components/ui/popover';
+import { cn } from '@/lib/utils'
+import { CalendarIcon } from 'lucide-react';
+import { Calendar } from '@/components/ui/calendar';
 
 const formSchema = z.object({
   title: z.string().min(2, {message: "Book must have at least a two-character title"}),
   description: z.string().optional(),
-  publishDate: z.coerce.date().max(new Date(), {message: "Date cannot be in future"}),
+  publishDate: z.date().max(new Date(), {message: "Date cannot be in future"}),
 })
 
 type FormSchemaType = z.infer<typeof formSchema>
@@ -99,9 +102,32 @@ function CreateBookPage() {
                   render={({field, fieldState})=>(
                     <FormItem>
                       <FormLabel>Publish Date</FormLabel>
-                      <FormControl>
-                        <Input type="date" {...field} value={field.value?.toString() || ''} />
-                      </FormControl>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button 
+                              variant="outline" 
+                              className={
+                                cn("w-full flex", !field.value && "text-gray-400")
+                              }
+                            >
+                              <div className="mr-auto">
+                                {field.value?.toLocaleDateString() || 'dd/MM/YYYY'}
+                              </div>
+                              <CalendarIcon className="h-4 w-4"/>
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger >
+                        <PopoverContent className="w-fit">
+                          <Calendar 
+                            mode="single" 
+                            selected={field.value} 
+                            onSelect={field.onChange} 
+                            disabled={date=> date > new Date()} 
+                            initialFocus
+                          />
+                        </PopoverContent>
+                      </Popover>
                       {fieldState.error ? (
                         <FormMessage />
                       ): (
