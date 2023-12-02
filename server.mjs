360258eb3aecd56b1c767d1c2bde7674c60c4253
Sayand Sathish <sayandcode@gmail.com>
2023-12-02 10:57:04 +0530

    Setup express as app server
    We detached from the remix-serve server. This gives us added flexibility

---
+++ server.mjs
@@ -0,0 +1,18 @@
+import express from 'express';
+import { createRequestHandler } from '@remix-run/express'
+import { broadcastDevReady } from '@remix-run/node'
+import * as build from './build/index.js';
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.all("*", createRequestHandler({ build }))
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+  if (process.env.NODE_ENV === 'development') {
+    broadcastDevReady(build);
+  }
+})
