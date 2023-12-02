955d0e84bdc44d52a7c629f1c8b5a3db0fddf27a
Sayand Sathish <sayandcode@gmail.com>
2023-12-02 19:04:40 +0530

    Add error routes
    404 and 500 error pages were added

--- app/root.tsx
+++ app/root.tsx
@@ -9,8 +9,8 @@ import {
   ScrollRestoration,
 } from '@remix-run/react';
 import twStylesheet from './tailwind.css';
-import fontsourceRedhatMonoStylesheet
-  from '@fontsource-variable/red-hat-mono/wght.css';
+import fontsourceRedhatMonoStylesheet from '@fontsource-variable/red-hat-mono/wght.css';
+import { TypographyH1, TypographyP } from './components/ui/Typography';
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
@@ -30,6 +30,30 @@ export default function App() {
       <body className="font-mono">
         <Outlet />
         <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary() {
+  return (
+    <html lang="en">
+      <head>
+        <title>Server Error</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="font-mono">
+        <div className="flex flex-col justify-center items-center h-screen">
+          <TypographyH1 className="text-5xl">Oops! This one's on us</TypographyH1>
+          <TypographyP className="mx-4 text-center">
+            Error 500: Something went wrong internally. Please try again later
+          </TypographyP>
+        </div>
         <Scripts />
         <LiveReload />
       </body>
---
+++ app/routes/$.tsx
@@ -0,0 +1,30 @@
+import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { TypographyH1, TypographyP } from '~/components/ui/Typography';
+
+export const meta: MetaFunction = ({ location }) => {
+  return [
+    { title: 'Invalid Route' },
+    { name: 'description', content: `${location.pathname} is not a valid route` },
+  ];
+};
+
+export const loader = ({ params }: LoaderFunctionArgs) => {
+  const path = params['*'];
+  if (!path) throw new Error('Something went wrong');
+  return json({ path }, { status: 404 });
+};
+
+export default function NotFound404() {
+  const { path } = useLoaderData<typeof loader>();
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <TypographyH1 className="text-5xl">Error 404</TypographyH1>
+      <TypographyP className="mx-4 text-center">
+        {`/${path} `}
+        route doesn't exist
+      </TypographyP>
+    </div>
+  );
+}
