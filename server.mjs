423f520fded401bff6050c7cab9ec42a7f466e56
Sayand Sathish <sayandcode@gmail.com>
2023-12-02 15:04:45 +0530

    Add shadcn ui and postcss

--- server.mjs
+++ server.mjs
@@ -1,13 +1,13 @@
 import express from 'express';
-import { createRequestHandler } from '@remix-run/express'
-import { broadcastDevReady } from '@remix-run/node'
+import { createRequestHandler } from '@remix-run/express';
+import { broadcastDevReady } from '@remix-run/node';
 import * as build from './build/index.js';
 
 const app = express();
 
 app.use(express.static('public'));
 
-app.all("*", createRequestHandler({ build }))
+app.all('*', createRequestHandler({ build }));
 
 const port = 3000;
 app.listen(port, () => {
@@ -15,4 +15,4 @@ app.listen(port, () => {
   if (process.env.NODE_ENV === 'development') {
     broadcastDevReady(build);
   }
-})
+});
