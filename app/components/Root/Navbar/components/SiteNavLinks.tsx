b99c7b11b4b2fe9f96a0bbda1902b420a5d16082
Sayand Sathish <sayandcode@gmail.com>
2023-12-22 18:23:13 +0530

    Modify navmenu to close on click
    Otherwise it remains open between navigations due to persisting in the root layout

--- app/components/Root/Navbar/components/SiteNavLinks.tsx
+++ app/components/Root/Navbar/components/SiteNavLinks.tsx
@@ -6,6 +6,7 @@ import {
 import { NavLink } from '@remix-run/react';
 import { MenuIcon } from 'lucide-react';
 import { Separator } from '~/components/ui/separator';
+import { useCallback, useState } from 'react';
 
 export default function SiteNavLinks() {
   return (
@@ -34,21 +35,32 @@ function DesktopLinks() {
 }
 
 function MobileLinks() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpenChange = useCallback(setIsOpen, [setIsOpen]);
+  const handleNavLinkClick = useCallback(() => setIsOpen(false), [setIsOpen]);
   return (
-    <Collapsible className="lg:hidden">
+    <Collapsible className="lg:hidden" open={isOpen} onOpenChange={handleOpenChange}>
       <CollapsibleTrigger className="flex items-center">
         <MenuIcon aria-label="Menu Icon" />
       </CollapsibleTrigger>
       <CollapsibleContent className="absolute bottom-0 left-0 translate-y-full w-full shadow-md">
         <ul className="bg-white w-full">
           <li>
-            <NavLink to="/catalog" className="p-2 block text-center hover:bg-gray-100 focus-visible:bg-gray-100 outline-none focus-visible:underline aria-[current='page']:bg-gray-700 aria-[current='page']:text-white aria-[current='page']:font-bold">
+            <NavLink
+              to="/catalog"
+              className="p-2 block text-center hover:bg-gray-100 focus-visible:bg-gray-100 outline-none focus-visible:underline aria-[current='page']:bg-gray-700 aria-[current='page']:text-white aria-[current='page']:font-bold"
+              onClick={handleNavLinkClick}
+            >
               Catalog
             </NavLink>
           </li>
           <Separator className="mx-auto w-11/12" />
           <li>
-            <NavLink to="/donate" className="p-2 block text-center hover:bg-gray-100 focus-visible:bg-gray-100 outline-none focus-visible:underline aria-[current='page']:bg-gray-700 aria-[current='page']:text-white aria-[current='page']:font-bold">
+            <NavLink
+              to="/donate"
+              className="p-2 block text-center hover:bg-gray-100 focus-visible:bg-gray-100 outline-none focus-visible:underline aria-[current='page']:bg-gray-700 aria-[current='page']:text-white aria-[current='page']:font-bold"
+              onClick={handleNavLinkClick}
+            >
               Donate
             </NavLink>
           </li>
