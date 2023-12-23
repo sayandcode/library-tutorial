e4e5dcf4ad850c210cda13123d2d1e265245889b
Sayand Sathish <sayandcode@gmail.com>
2023-12-23 12:39:36 +0530

    Refactor and Add login button fallback for noscript environments
    If there's no script, a normal link is rendered. Otherwise a modal-opening button

--- app/components/Root/Navbar/components/NavbarLoginBtn/index.tsx
+++ app/components/Root/Navbar/components/NavbarLoginBtn/index.tsx
@@ -1,33 +1,23 @@
-import { useCallback, useState } from 'react';
-import { Button } from '~/components/ui/button';
+import { Link } from '@remix-run/react';
+import { useCallback, useLayoutEffect, useState } from 'react';
+import { Button, buttonVariants } from '~/components/ui/button';
 import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger, DialogHeader } from '~/components/ui/dialog';
+import { cn } from '~/lib/utils';
 
 export default function NavbarLoginBtn(
   { className }:
   { className?: HTMLDivElement['className'] },
 ) {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleOpenChange = useCallback((newIsOpen: boolean) => {
-    // setup close via interaction with modal
-    if (!newIsOpen) {
-      window.history.back(); // this automatically closes the modal
-      return;
-    }
-    setIsOpen(true);
-    window.history.pushState({}, '', '/login');
-
-    // setup close via browser back button
-    window.addEventListener('popstate', () => {
-      if (window.location.pathname !== '/login') {
-        setIsOpen(false);
-      }
-    }, { once: true });
-  }, []);
+  const [isOpen, handleOpenChange] = useDialogOpenHandler();
+  const isJsLoginButtonHidden = useIsJsLoginButtonHidden();
   return (
     <div className={className}>
+      <noscript>
+        <Link to="/login" className={cn(buttonVariants({ variant: 'default' }), 'font-bold')}>Login</Link>
+      </noscript>
       <Dialog open={isOpen} onOpenChange={handleOpenChange}>
         <DialogTrigger asChild>
-          <Button className="font-bold">Login</Button>
+          <div hidden={isJsLoginButtonHidden}><Button className="font-bold">Login</Button></div>
         </DialogTrigger>
         <DialogContent>
           <DialogHeader>
@@ -45,4 +35,35 @@ export default function NavbarLoginBtn(
       </Dialog>
     </div>
   );
+}
+
+function useIsJsLoginButtonHidden() {
+  const [isHidden, setIsHidden] = useState(true);
+  useLayoutEffect(() => {
+    setIsHidden(false);
+  }, []);
+
+  return isHidden;
+}
+
+function useDialogOpenHandler() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpenChange = useCallback((newIsOpen: boolean) => {
+    // setup close via interaction with modal
+    if (!newIsOpen) {
+      window.history.back(); // this automatically closes the modal
+      return;
+    }
+    setIsOpen(true);
+    window.history.pushState({}, '', '/login');
+
+    // setup close via browser back button
+    window.addEventListener('popstate', () => {
+      if (window.location.pathname !== '/login') {
+        setIsOpen(false);
+      }
+    }, { once: true });
+  }, []);
+
+  return [isOpen, handleOpenChange] as const;
 }
