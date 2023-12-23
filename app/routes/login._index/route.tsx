9887526dd2e2f7d691b06a047f8d2683398cac90
Sayand Sathish <sayandcode@gmail.com>
2023-12-23 17:26:56 +0530

    Add pages to login and signup routes

--- app/routes/login._index/route.tsx
+++ app/routes/login._index/route.tsx
@@ -1,7 +1,39 @@
 import type { ActionFunctionArgs } from '@remix-run/node';
+import LoginForm from '~/components/Auth/LoginForm';
+import { TypographyP } from '~/components/ui/Typography';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '~/components/ui/card';
+import { Link } from '@remix-run/react';
 
 export async function action(args: ActionFunctionArgs) {
   // TODO: Handle login request
   console.log(args);
   return null;
+}
+
+export default function LoginRoute() {
+  return (
+    <div className="m-8">
+      <Card className="max-w-lg mx-auto">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <LoginForm />
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <TypographyP className="text-center">
+            Don't have an account?
+            {' '}
+            <Link to="/sign-up" className="underline hover:opacity-50">Sign Up</Link>
+          </TypographyP>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
--- app/routes/sign-up._index/route.tsx
+++ app/routes/sign-up._index/route.tsx
@@ -1,7 +1,39 @@
 import type { ActionFunctionArgs } from '@remix-run/node';
+import { TypographyP } from '~/components/ui/Typography';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '~/components/ui/card';
+import { Link } from '@remix-run/react';
+import SignUpForm from '~/components/Auth/SignUpForm';
 
 export async function action(args: ActionFunctionArgs) {
   // TODO: Handle login request
   console.log(args);
   return null;
+}
+
+export default function LoginRoute() {
+  return (
+    <div className="m-8">
+      <Card className="max-w-lg mx-auto">
+        <CardHeader>
+          <CardTitle>Sign Up</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <SignUpForm />
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <TypographyP className="text-center">
+            Already have an account?
+            {' '}
+            <Link to="/login" className="underline hover:opacity-50">Login</Link>
+          </TypographyP>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
